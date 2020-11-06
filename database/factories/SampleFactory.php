2e8497c40ba3691db224a749b6872c8f6b12f517
Michael Geofrey Dela Rama <mgdelarama.28@gmail.com>
2020-11-06 20:42:59 +0800

    Removed sample factory

--- database/factories/SampleFactory.php
+++ database/factories/SampleFactory.php
@@ -3,12 +3,12 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Sample;
-use Faker\Generator as Faker;
+//  use Faker\Generator as Faker;
 
-$factory->define(Sample::class, function (Faker $faker) {
-    return [
-        'name' => $faker->word,
-        'image_path' => null,
-        'description' => $faker->text($maxNbChars = 100),
-    ];
-});
+// $factory->define(Sample::class, function (Faker $faker) {
+//     return [
+//         'name' => $faker->word,
+//         'image_path' => null,
+//         'description' => $faker->text($maxNbChars = 100),
+//     ];
+// });
