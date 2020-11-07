6297024232dd22afd15b1af8bc1a8c6852c6a3c9
Michael Geofrey Dela Rama <mgdelarama.28@gmail.com>
2020-11-07 17:42:56 +0800

    Added favicon

--- resources/views/web/partials/styles.blade.php
+++ resources/views/web/partials/styles.blade.php
@@ -1,14 +1,10 @@
+<link rel="icon" href="{{ secure_asset('images/favicon.png') }}" type="image/x-icon"/>
 <link href="https://fonts.googleapis.com/css2?family=Spectral:ital,wght@0,200;0,300;0,400;0,500;0,700;0,800;1,200;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
-
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
 <link rel="stylesheet" href="{{ secure_asset('css/web/animate.css') }}">
-
 <link rel="stylesheet" href="{{ secure_asset('css/web/owl.carousel.min.css') }}">
 <link rel="stylesheet" href="{{ secure_asset('css/web/owl.theme.default.min.css') }}">
 <link rel="stylesheet" href="{{ secure_asset('css/web/magnific-popup.css') }}">
-
 <link rel="stylesheet" href="{{ secure_asset('css/web/style.css') }}">
 <link rel="stylesheet" href="{{ secure_asset('css/web/flaticon.css') }}">
-
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.min.css">
