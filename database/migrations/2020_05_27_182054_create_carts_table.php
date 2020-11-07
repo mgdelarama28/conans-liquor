65fb9b8ffbb5917c339e2011bda0e876123f77ec
Michael Geofrey Dela Rama <mgdelarama.28@gmail.com>
2020-11-07 09:25:48 +0800

    Removed discount and delivery_fee fields from carts table

--- database/migrations/2020_05_27_182054_create_carts_table.php
+++ database/migrations/2020_05_27_182054_create_carts_table.php
@@ -18,9 +18,6 @@ class CreateCartsTable extends Migration
             $table->string('session_id')->nullable();
             $table->boolean('status')->default(1); /** 0 = Inactive / 1 = Active */
             $table->decimal('subtotal', 9, 2)->default(0.00);
-            $table->decimal('delivery_fee', 9, 2)->default(0.00);
-            $table->unsignedInteger('discount_percentage')->default(0.00);
-            $table->decimal('discount_amount', 9, 2)->default(0.00);
             $table->decimal('total', 9, 2)->default(0.00);
             $table->timestamps();
         });
